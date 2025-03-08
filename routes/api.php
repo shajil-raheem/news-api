6c247dfd7420356b726b22dd2d5fd25e21350c8d
shajilabdurahiman <shajil@zohomail.in>
2025-03-08 07:24:47 +0530

    Initial Commit

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('/register', [UserController::class, 'store']);
+Route::apiResource('/user', UserController::class)->exept(['index', 'store'])->middleware('auth:sanctum');
+
